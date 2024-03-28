730121b92894f74cd6431373ffebf04509907d87
Steve Roques <steve.roques@gmail.com>
2024-03-28 15:04:47 +0100

    formating with resume

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use reqwest;
 use embryo::{EmbryoList, EmPair};
+use textwrap::wrap;
 
 #[tokio::main]
 async fn main() {
@@ -26,15 +27,30 @@ async fn main() {
         match filter_response {
             Ok(filter_response) => {
                 for embryo in filter_response.embryo_list {
+                    let mut url = String::new();
+                    let mut resume = String::new();
                     for pair in &embryo.properties {
-                        if pair.name == "url" {
-                            let em_pair = EmPair {
-                                name: pair.name.clone(),
-                                value: pair.value.clone(),
-                            };
-                            println!("{:?}", em_pair);
+                        match pair.name.as_str() {
+                            "url"  => {
+                                url=pair.value.clone();
+                            },
+                            "resume" => {
+                                resume=pair.value.clone();
+                            }
+                            _ => { }
                         }
                     }
+                    let term_width = match term_size::dimensions() {
+                        Some((w, _)) => w as usize,
+                        None => 80,
+                    };
+
+                    let wrapped_resume = wrap(&resume, term_width - 1)
+                        .iter()
+                        .map(|line| format!("\t{}", line))
+                        .collect::<Vec<_>>()
+                        .join("\n");
+                    println!("{}\n{}", url, wrapped_resume);
                 }
             }
             Err(_) => {
